e062a0f3dd9ef5f6dfa1dbc246c0cd8d15c833e6
Naoya Teraoka <59727299+Naoya323150403@users.noreply.github.com>
2022-12-24 02:11:49 +0900

    2022/12/23

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButtons";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SarchInput } from "./components/molecules/SearchInput";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <PrimaryButton>Test</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SarchInput />
     </div>
   );
 }
